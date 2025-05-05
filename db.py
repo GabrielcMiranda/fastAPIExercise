a20cea398196eaeb805d5ce945ef48f9523d5155
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-05-05 10:58:32 -0300

    fix: ajuste na oonexao com banco

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 from os import getenv
-from sqlalchemy.ext.asyncio import asynSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 url = getenv('DB_URL')
+
 engine = create_async_engine(url)
+session = sessionmaker(engine, Class=AsyncSession)
